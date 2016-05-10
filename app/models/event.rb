a339f652382a98f473ac8c3eb0915983e91472d7
yehong you <youyehong@gmail.com>
2016-05-10 11:20:50 +1000

    model with relation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+  belongs_to :genre
+  belongs_to :venue
+  has_many :artists
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,6 @@
 class Venue < ActiveRecord::Base
+   has_many :events
+   has_many :comments
+   has_many :likes
+   belongs_to :user 
 end
