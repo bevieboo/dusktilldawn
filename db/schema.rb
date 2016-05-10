f5b0ae2936ff2345078c48c6114c9fc39d29441c
yehong you <youyehong@gmail.com>
2016-05-10 11:53:03 +1000

    add description to venues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510005730) do
+ActiveRecord::Schema.define(version: 20160510015006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,8 +84,9 @@ ActiveRecord::Schema.define(version: 20160510005730) do
     t.text     "image"
     t.string   "type"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   add_foreign_key "artists", "events"
