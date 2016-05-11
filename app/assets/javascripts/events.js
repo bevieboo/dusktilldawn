e8bde314f2f8d28439cf567c18c44e1e5282e215
yehong you <youyehong@gmail.com>
2016-05-11 14:48:10 +1000

    fix conflict

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -8,9 +8,8 @@ $( document ).on('ready', function() {
       }).done(function(events) {
 
         $.each(events, function(index, event) {
-          console.log( $('#events-template'));
           var template = $('#events-template').html();
-          console.log(template);
+          console.log(events);
           var templateFunction = Handlebars.compile(template);
 
           if (event.image == undefined) {
@@ -22,9 +21,9 @@ $( document ).on('ready', function() {
           var html = templateFunction({
             name: event.name,
             id: event.id,
-            type: event.genre_id.name,
+            type: event.venue.venue_type,
             image_url: venueImage,
-            venue: event.venue_id,
+            venue: event.venue.name,
             like_count: event.like_count
           });
 
@@ -38,45 +37,45 @@ $( document ).on('ready', function() {
 
   showEvents();
 
-  $('.list').on('click', '.item', function(event) {
-
-    $('.list').empty();
-    var $item = $(event.target).closest('.item');
-    var venueId = $item.data('venue-id');
-    console.log(venueId);
-
-    $.ajax({
-      url: 'http://localhost:3000/api/venues/' + venueId
-    }).done(function(venue) {
-      console.log(venue);
-
-      var template = $('#venue-details-template').html();
-      var templateFunction = Handlebars.compile(template);
-
-      if (venue.image == undefined) {
-        venueImage = "http://www.molotowcocktail.eu/images/slider-img4.jpg"
-      } else {
-        venueImage = venue.image
-      };
-      console.log(venue.image);
-
-      var html = templateFunction({
-        name: venue.name,
-        id: venue.id,
-        desc: venue.description,
-        type: venue.venue_type.toUpperCase(),
-        image_url: venueImage,
-        address: venue.address,
-        suburb: venue.suburd,
-        like_count: venue.like_count
-      });
-
-      var $newDiv = $(html);
-      $('.list').append($newDiv);
-
-    })
-    console.log(this);
-  })
+  // $('.list').on('click', '.item', function(event) {
+  //
+  //   $('.list').empty();
+  //   var $item = $(event.target).closest('.item');
+  //   var venueId = $item.data('venue-id');
+  //   console.log(venueId);
+  //
+  //   $.ajax({
+  //     url: 'http://localhost:3000/api/venues/' + venueId
+  //   }).done(function(venue) {
+  //     console.log(venue);
+  //
+  //     var template = $('#venue-details-template').html();
+  //     var templateFunction = Handlebars.compile(template);
+  //
+  //     if (venue.image == undefined) {
+  //       venueImage = "http://www.molotowcocktail.eu/images/slider-img4.jpg"
+  //     } else {
+  //       venueImage = venue.image
+  //     };
+  //     console.log(venue.image);
+  //
+  //     var html = templateFunction({
+  //       name: venue.name,
+  //       id: venue.id,
+  //       desc: venue.description,
+  //       type: venue.venue_type.toUpperCase(),
+  //       image_url: venueImage,
+  //       address: venue.address,
+  //       suburb: venue.suburd,
+  //       like_count: venue.like_count
+  //     });
+  //
+  //     var $newDiv = $(html);
+  //     $('.list').append($newDiv);
+  //
+  //   })
+  //   console.log(this);
+  // })
 
    $(document).on('click', '.back', function() {
     $('.list').empty();
