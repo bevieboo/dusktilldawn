ad05a549729f2b8209b2c0aefb6bbe17ad80cb5d
yehong you <youyehong@gmail.com>
2016-05-13 12:55:58 +1000

    sort time function

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -17,13 +17,20 @@ $(document).on('ready', function() {
             eventImage = event.image
           };
 
+         var monthsArr =[ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+         var date = event.event_time;
+         console.log(date);
+         var string = new Date(date);
+         var month = (monthsArr[string.getMonth()]);
+         var new_date = (string.getDay()+ " " + month + " " + string.getFullYear()+ ",  " + string.getHours()+ ":" + string.getMinutes());
+
           var html = templateFunction({
             name: event.name,
             id: event.id,
             type: event.venue.venue_type,
             image_url: eventImage,
             venue: event.venue.name,
-            event_time: event.event_time,
+            event_time: new_date,
             like_count: event.like_count
           });
 
@@ -36,6 +43,56 @@ $(document).on('ready', function() {
   }
 
   showEvents();
+
+    //  sort events function
+    $('#sortBtn').on('click', function(){
+        $('.list-all-events').empty();
+        $.ajax({
+          url: 'http://localhost:3000/api/events'
+        }).done(function(events){
+            var orderEvents = _.sortBy(events, 'event_time');
+
+            $.each(orderEvents, function(index, event) {
+              var template = $('#events-template').html();
+              var templateFunction = Handlebars.compile(template);
+
+              if (event.image == undefined) {
+                eventImage = "http://www.molotowcocktail.eu/images/slider-img4.jpg"
+              } else {
+                eventImage = event.image
+              };
+
+              var monthsArr =[ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+              var date = event.event_time;
+              console.log(date);
+              var string = new Date(date);
+              var month = (monthsArr[string.getMonth()]);
+              var new_date = (string.getDay()+ " " + month + " " + string.getFullYear()+ ",  " + string.getHours()+ ":" + string.getMinutes());
+
+              var html = templateFunction({
+                name: event.name,
+                id: event.id,
+                type: event.venue.venue_type,
+                image_url: eventImage,
+                venue: event.venue.name,
+                event_time: new_date,
+                like_count: event.like_count
+              });
+
+              var $newDiv = $(html);
+              $('.list-all-events').append($newDiv);
+            });
+
+        })
+
+    });
+
+
+
+
+
+
+
 
 
     $(document).on('click', '.back', function() {
