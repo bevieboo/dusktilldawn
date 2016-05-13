d2bd64bbb48d306b720aca899907d6d4f4e6dfd7
bevieboo <bevin.sng@gmail.com>
2016-05-13 12:20:42 +1000

    icwhef

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -19,27 +19,34 @@ class VenuesController < ApplicationController
     venue.user_id = current_user.id
     venue.description = params[:description]
     venue.save
+    redirect_to "/users/" + venue.user_id.to_s
   end
 
   def show
     @venue = Venue.find(params[:id])
   end
 
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
   def update
-    venue.name = params[:name]
-    venue.address = params[:address]
-    venue.suburd = params[:suburd]
-    venue.postcode = params[:postcode]
-    venue.venue_type = params[:venue_type]
-    venue.image = params[:image]
-    venue.user_id = params[:user_id]
-    venue.description = params[:description]
-    venue.save
+    @venue = Venue.find(params[:id])
+    @venue.name = params[:name]
+    @venue.address = params[:address]
+    @venue.suburd = params[:suburb]
+    @venue.postcode = params[:postcode]
+    @venue.venue_type = params[:venue_type]
+    @venue.image = params[:image]
+    @venue.description = params[:description]
+    @venue.save
+    redirect_to "/venues/#{params[:id]}"
   end
 
   def destroy
-    venue = Venue.find(params[:id])
-    venue.destroy
+    @venue = Venue.find(params[:id])
+    @venue.destroy
+    redirect_to "/users/#{params[:id]}"
   end
 
 end
